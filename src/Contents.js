6d846185a815350a3c174a3c69e625166726ac45
Rahul Ram P <98167864+rahulram01@users.noreply.github.com>
2024-01-13 17:53:32 +0530

    Create Contents.js

---
+++ src/Contents.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contents() {
+  return (
+    <div>
+      <h1>Your Content Goes Here</h1>
+      <p>This is a sample content component below the navbar.</p>
+    </div>
+  );
+}
+
+export default Contents;
